fed046adf53fae3a6fe07d68c7b5275dcb010766
ViniciusChagas <viniciushrcs@gmail.com>
2021-04-14 17:20:39 -0300

    Setando endereços de assinatura para a rede

--- contracts/Tracking.sol
+++ contracts/Tracking.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.5.10;
 
 contract Tracking {
 
-	enum Step { Supplier, SupplierToFactory, Factory, FactoryToStore }
+	enum Step { Supplier, SupplierToFactory, Factory, FactoryToStore, StoreVerification}
 
 	//State of each step
 	//Unsigned Integer - no solidity não temos sinais negativos
@@ -19,14 +19,29 @@ contract Tracking {
 	uint8 public supplierToFactoryState;
 	uint8 public factoryState;
 	uint8 public factoryToStoreState;
+	uint8 public storeVerificationState;
 
 	//Current Step
 	// Will change over time
 	Step public currentStep = Step.Supplier;
 
+	//Roles
+	//address - controle de endereços
+	address public supplier;
+	address public factory;
+	address public store;
+
+	//Contract creation
+	constructor(address _supplier, address _factory, address _store) {
+		supplier = _supplier;
+		factory = _factory;
+		store = _store;
+	}
+
 	//public é um modificador, para controlar acesso.
 	function next() public {
  	  if (currentStep == Step.Supplier) {
+ 	  	if(msg.sender != supplier) revert("User not allowed")
  	  	supplierState++;
  	    if (supplierState == Finished) {
  	    	currentStep = Step.SupplierToFactory;
@@ -34,6 +49,7 @@ contract Tracking {
  	    return;
  	  }
  	  if (currentStep == Step.SupplierToFactory) {
+ 	  	if(msg.sender != supplier) revert("User not allowed")
  	  	supplierToFactoryState++;
  	  	if (supplierToFactoryState == Finished) {
  	  		currentStep = Step.Factory;
@@ -41,6 +57,7 @@ contract Tracking {
  	  	return;
  	  }
  	   if (currentStep == Step.Factory) {
+ 	  	if(msg.sender != factory) revert("User not allowed")
  	  	factoryState++;
  	  	if (factoryState == Finished) {
  	  		currentStep = Step.FactoryToStore;
@@ -48,11 +65,20 @@ contract Tracking {
  	  	return;
  	  }
  	   if (currentStep == Step.FactoryToStore) {
+ 	  	if(msg.sender != factory) revert("User not allowed")
+ 	  	factoryToStoreState++;
  	  	if (factoryToStoreState == Finished) {
+ 	  		currentStep = Step.StoreVerification;
+ 	  	}
+ 	  	return;
+ 	  }
+ 	   if (currentStep == Step.StoreVerification) {
+ 	  	if(msg.sender != store) revert("User not allowed")
+ 	  	if (storeVerificationState == Finished) {
  	  		//Revert é uma forma de encerrar o código e lançar um erro para o usuário
  	  		revert("Process is already finished");
  	  	}
- 	  	factoryToStoreState++;
+ 	  	storeVerificationState++;
  	  	return;
  	  }
 	}
